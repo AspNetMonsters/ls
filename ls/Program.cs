67c341f232e2988e5af22e644c6499300ddae902
Simon Timms <stimms@gmail.com>
2016-08-01 13:23:41 -0600

    Initial

---
+++ ls/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ls
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string workingDirectory = args.Length == 0 ? "." : args[0];
+            DirectoryInfo di = new DirectoryInfo(workingDirectory);
+            int maxLength = di.GetFiles().Select(x => x.Name.Length).Max();
+            foreach(var file in di.GetFiles().OrderBy(x=>x.Name))
+            {
+                Console.WriteLine($"{file.Name.PadRight(maxLength)}\t{file.CreationTime}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
